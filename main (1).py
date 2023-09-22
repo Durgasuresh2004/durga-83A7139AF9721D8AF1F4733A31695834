7b221e2bb3d22f6f53dd6ad05a318e80c7cab7f6
Durgasuresh2004 <144885456+Durgasuresh2004@users.noreply.github.com>
2023-09-22 14:33:22 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,19 @@
+
+#function defined
+def leep year check(input year):
+if (inpt-year%4)==0:
+  if(input-year%100)==0:
+    if(input-year%400)==0:
+       return type
+    else:
+       return False
+    else:
+       return True
+    else:
+       return False
+#Driver code
+input-year=int(input("Enter tha yearnto be chedcked:"))
+if(leepyear chack(nput-year));
+ print("leep year")
+else:
+print("Not a leep year")
